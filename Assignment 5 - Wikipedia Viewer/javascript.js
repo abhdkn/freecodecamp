b2389214fc0f5ed04e7e3b35914a9f9717ce399b
Abh Dkn <flysimply@gmail.com>
2018-02-21 23:06:20 -0800

    removed commented lines

--- Assignment 5 - Wikipedia Viewer/javascript.js
+++ Assignment 5 - Wikipedia Viewer/javascript.js
@@ -1,9 +1,6 @@
 function getWikiList() {
     var url = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&search=" + q.value + "&namespace=0&format=json";
-    //var url = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&search=Sacramento&namespace=0&format=json";
 
-
-    //var url = "https://random-quote-generator.herokuapp.com/api/quotes/random";
     $.getJSON(url, function (response) {
         console.log("json:", response);
         $(".wikiList").empty();
@@ -17,14 +14,12 @@ function getWikiList() {
 
 function randomWikiList() {
     var url = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnlimit=10";
-    //var url = "https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=opensearch&search=Sacramento&namespace=0&format=json";
 
 
-    //var url = "https://random-quote-generator.herokuapp.com/api/quotes/random";
     $.getJSON(url, function (response) {
         console.log("json:", response);
         $(".wikiList").empty();
-        for (var i = 0; i < 5; i++) {
+        for (var i = 0; i < 10; i++) {
           //alert(response.query.random[0].id);
             $(".wikiList").append("<div class='card'><p><a target='_blank' href=http://en.wikipedia.org/?curid=" + response.query.random[i].id + ">" + response.query.random[i].title + "</a></p><p class='description'></></p></div>");
         }
